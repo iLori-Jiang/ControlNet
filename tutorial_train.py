28bec3f735885dc987d5b0f983e7feb135a0ff43
Haiyang <haiyang@ampere.lix.polytechnique.fr>
2024-06-17 17:53:28 +0200

    modifying the code

--- tutorial_train.py
+++ tutorial_train.py
@@ -1,6 +1,7 @@
 from share import *
 
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import DataLoader
 from tutorial_dataset import MyDataset
 from cldm.logger import ImageLogger
@@ -8,10 +9,14 @@ from cldm.model import create_model, load_state_dict
 
 
 # Configs
-resume_path = './models/control_sd15_ini.ckpt'
+model_name = 'control_toy_sd15_ini-epoch=0-step=7899'
+resume_path = './models/' + model_name + '.ckpt'
 batch_size = 4
+accumulate_grad_batches = 4
 logger_freq = 300
 learning_rate = 1e-5
+training_samples = 4000*4
+train_steps = training_samples / batch_size
 sd_locked = True
 only_mid_control = False
 
@@ -28,7 +33,31 @@ model.only_mid_control = only_mid_control
 dataset = MyDataset()
 dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
-trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+
+
+# 自定义回调以确保在训练结束时保存模型
+class SaveCheckpointOnFinish(pl.Callback):
+    def on_train_end(self, trainer, pl_module):
+        trainer.save_checkpoint("lightning_logs/" + model_name + "-last.ckpt")
+
+
+# 设置检查点回调，保存最佳模型和每个epoch末尾的模型
+checkpoint_callback = ModelCheckpoint(
+    dirpath='lightning_logs/',
+    filename= model_name + '-{epoch}-{step}',
+    save_top_k=1,
+    every_n_train_steps = logger_freq
+)
+
+# trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
+trainer = pl.Trainer(
+            max_steps=train_steps,
+            accumulate_grad_batches=accumulate_grad_batches,
+            devices=[0], 
+            accelerator='gpu', 
+            precision=32, 
+            callbacks=[logger, checkpoint_callback, SaveCheckpointOnFinish()]
+        )
 
 
 # Train!
